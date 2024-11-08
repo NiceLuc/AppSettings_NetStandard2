0c7d23f2d1bd552da25662748a9c3b3865bff4d2
Luc Hardbarger <luc.hardbarger@nice.com>
2024-11-08 15:23:19 -0500

    Structure the project around DI for both NET48 and NET8.
    Shows different approaches from the different frameworks resulting in the same functionality using netstandard2 interfaces.

---
+++ Service_8.0/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SharedLibrary;
+
+namespace Service_80;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IAppSettings, AppSettingsWrapper>();
+        services.AddSingleton<IConnectionStrings, ConnectionStringsWrapper>();
+        services.AddSingleton<SystemSettingsReport>();
+        return services;
+    }
+}
--- Service_8.0/Program.cs
+++ Service_8.0/Program.cs
@@ -5,42 +5,57 @@ using ConfigurationManager = System.Configuration.ConfigurationManager;
 using Service_80;
 using SharedLibrary;
 
-var builder = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration((context, config) =>
-    {
-        // Add appsettings.json settings
-        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-        // Add Machine.config
-        var machineConfigPath = Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.System),
-            @"..\Microsoft.NET\Framework\v4.0.30319\Config\Machine.config");
-
-        if (File.Exists(machineConfigPath)) 
-            config.AddXmlFile(machineConfigPath, optional: false, reloadOnChange: true);
-
-        // Add environment variables
-        config.AddEnvironmentVariables();
-
-        // Add app.config settings
-        var appConfig = ConfigurationManager.AppSettings;
-        if (appConfig is {Count: > 0})
-        {
-            var appConfigDict = new Dictionary<string, string>();
-            foreach (var key in appConfig.AllKeys)
-            {
-                appConfigDict[key ?? ""] = appConfig[key] ?? string.Empty;
-            }
-
-            config.AddInMemoryCollection(appConfigDict!);
-        }
-    });
-
-var host = builder.Build();
-var configuration = host.Services.GetRequiredService<IConfiguration>();
+var provider = ConfigureServices(args, services =>
+{
+    // configure DI
+    services.AddApplicationServices();
+});
 
 Console.WriteLine("Reading appsettings.json file (.NET 8.0)");
+var report = provider.GetRequiredService<SystemSettingsReport>(); // <== netstandard2.0 type
+report.WriteSettings();
 
-var appSettings = new AppSettingsWrapper(configuration);
-var connectionStrings = new ConnectionStringsWrapper(configuration);
-SettingsConsumer.WriteSettings(appSettings, connectionStrings);
+#region Boilerplate Code
+
+static IServiceProvider ConfigureServices(string[] args, Action<IServiceCollection>? configure)
+{
+    var builder = Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((context, config) =>
+        {
+            // Add environment variables
+            config.AddEnvironmentVariables();
+
+            // Add (.NET8) appsettings.json settings
+            config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            // Add (.NET48) machine.config settings
+            var machineConfigPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.System),
+                @"..\Microsoft.NET\Framework\v4.0.30319\Config\Machine.config");
+
+            if (File.Exists(machineConfigPath))
+                config.AddXmlFile(machineConfigPath, optional: false, reloadOnChange: true);
+
+            // Add (.NET48) app.config settings
+            var appConfig = ConfigurationManager.AppSettings;
+            if (appConfig is {Count: > 0})
+            {
+                var appConfigDict = new Dictionary<string, string>();
+                foreach (var key in appConfig.AllKeys)
+                {
+                    appConfigDict[key ?? ""] = appConfig[key] ?? string.Empty;
+                }
+
+                config.AddInMemoryCollection(appConfigDict!);
+            }
+        })
+        .ConfigureServices((context, services) =>
+        {
+            configure?.Invoke(services);
+        });
+
+    var host = builder.Build();
+    return host.Services;
+}
+
+#endregion
